d4db5dc5b047ab417fd5447df22bb01a777ff80d
noliva <luis.oliva@est.fi.uncoma.edu.ar>
2026-01-08 11:20:08 -0300

    Agregar archivos de proyecto.

---
+++ Controlador/Menu.cs
@@ -0,0 +1,83 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoConsolaV2.Controlador
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+        }
+        //Recuperamos la lista de objetos de json apenas empieza la app
+
+        
+
+        public void iniciarApp()
+        {
+            bool terminar = false;
+            string eleccion;
+
+            do
+            {
+
+                eleccion = AnsiConsole.Prompt(
+                        new SelectionPrompt<String>()
+                        .Title("[Green]Elija una opción[/]")
+                        .AddChoices(
+                            "Ver Objetos de la tienda",
+                            "Agregar Objeto",
+                            "Sacar Objeto",
+                            "Editar Objeto",
+                            "Guardar Cambios",
+                            "Finalizar"
+                        )
+                    );
+
+                switch (eleccion)
+                {
+
+                    case "Ver Objetos de la ":
+                        break;
+                    case "Agregar Objeto":
+                        break;
+                    case "Sacar Objeto":
+                        ; break;
+                    case "Editar Objeto":
+                        break;
+                    case "Guardar Cambios":
+                        
+                        break;
+                    case "Finalizar":
+                        terminar = true;
+                        break;
+
+                    default:
+                        AnsiConsole.Clear();
+                        AnsiConsole.MarkupLine("[Red] Presione un valor correcto [/]");
+                        break;
+                }
+            } while (!terminar);
+
+
+        }
+
+        public void modificarObjeto()
+        {
+            
+        }
+
+        public void crearObjeto()
+        {
+           
+        }
+
+        public void sacarObjeto()
+        {
+            
+        }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using ProyectoConsolaV2.Controlador;
+using Spectre.Console;
+
+namespace ProyectoEximoConsola
+{
+    public class Inicio
+    {
+        static void Main(string[] arg)
+        {
+            Menu inicio = new Menu();
+            //AnsiConsole.Progress().Start(fun =>
+            //{
+            //    var tarea = fun.AddTask("[blue]Cargando aplicación[/]");
+
+            //    while (!fun.IsFinished)
+            //    {
+            //        tarea.Increment(2);
+            //        Thread.Sleep(50);
+            //    }
+            //});
+
+            AnsiConsole.Clear();
+            var intro = new Text("#####################################################\n" +
+                                "#------- ¡Bienvenidos a la tienda Nahuelito! -------#\n" +
+                                "#####################################################\n", new Style(foreground: Color.Green)).Centered();
+            AnsiConsole.Write(intro);
+
+            inicio.iniciarApp();
+
+            var fin = new Text("#####################################################\n" +
+                                "#------- !Gracias Vuelvas Prontoss! -------#\n" +
+                                "#####################################################\n", new Style(foreground: Color.Yellow)).Centered();
+            AnsiConsole.Write(fin);
+
+        }
+    }
+}
